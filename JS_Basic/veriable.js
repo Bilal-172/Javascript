a62b48194a30de4cbf4b80a516e3e4f0792700ce
Muhammad Bilal <119156465+Bilal-172@users.noreply.github.com>
2023-07-09 17:33:32 +0000

    Chnage folder and write

---
+++ JS_Basic/veriable.js
@@ -0,0 +1,11 @@
+const accountId = 111448
+let accountEmail =  "g.bilal034@gmail.com"
+var accountpassword = "1145444"
+accountcity = "CCW "
+
+// accountId = 2 
+accountEmail = "bilal@gmail.com"
+accounttpassword = "2245245"
+accountcity = "Ali"
+console.log(accountId);
+console.table([accountId, accountpassword, accountcity, accountEmail])
